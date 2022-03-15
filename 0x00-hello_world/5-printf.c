1fa37d460b56ed722ce2fb69a78b7e56dda3a33b
yared2112 <teklewold.yared21@gmail.com>
2022-03-14 18:27:26 -0700

    5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
