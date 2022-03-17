ced6cd5f1d55b979232a95b60cf923a9d6df1b43
yared2112 <teklewold.yared21@gmail.com>
2022-03-16 23:54:44 -0700

    my11st commited file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - Draws a triangle line in the terminal
+ * _putchar - Prints characters one by one
+ * @size: Parameter of size
+ *
+ * Description: Draws a triangle
+ * Return: 0 value
+ */
+void print_triangle(int size)
+{
+int y, z;
+
+if (size >  0)
+{
+for (z = 1; z <= size; z++)
+{
+for (y = 1; y <= size; y++)
+{
+if (y <= size - z)
+_putchar(' ');
+else
+_putchar('#');
+
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
