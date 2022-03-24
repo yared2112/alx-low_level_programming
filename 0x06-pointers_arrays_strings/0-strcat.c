700baeafb8129cc3de334d0bbc4816478be8529f
yared2112 <teklewold.yared21@gmail.com>
2022-03-23 17:54:00 -0700

    0-strcat.c files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,22 @@
 #include "main.h"
-
 /**
- * _strncat -appends src to the dest string
+ * _strcat - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
- * @n: largest number of bytes to append
  *
  * Return: address of dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 int i, j;
 
 i = j = 0;
 while (*(dest + i))
 i++;
-while (j < n && *(src + j))
+while ((*(dest + i) = *(src + j)))
 {
-*(dest + i) = *(src + j);
 i++;
 j++;
 }
-if (j < n)
-*(dest + i) = *(src + j);
 return (dest);
 }
