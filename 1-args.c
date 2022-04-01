64ce3a76df45b05713637be6827175c6cef3e745
yared2112 <teklewold.yared21@gmail.com>
2022-04-01 06:17:57 -0700

    1-args.c files

--- 1-args.c
+++ 1-args.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints the name number of arguments passed to it
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- *
- * Return: always 0
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+
+int main(int argc, char *argv[])
 {
-printf("%d\n", argc - 1);
-return (0);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
+	return (0);
 }
