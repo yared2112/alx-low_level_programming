e039d33805cfabe1d58a910f627e2dbb7f5e9a50
yared2112 <teklewold.yared21@gmail.com>
2022-04-01 16:39:03 -0700

    100-change files

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,9 +1,8 @@
-
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include "main.h"
 
 /**
  * coinConverter - Helper function that does all the mathematics
