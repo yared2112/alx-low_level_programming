8b49611bf8bd9cf90073665cf00a82bc8c3d6b02
yared2112 <teklewold.yared21@gmail.com>
2022-04-06 15:08:00 -0700

    completed task

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
-#define MAIN_H
+#definr MAIN_H
+
+/*
+ * File: holberton.h
+ * Auth: Firdaus
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libholberton.a.
+ */
 
 int _putchar(char c);
 int _islower(int c);
@@ -11,7 +18,8 @@ int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcatt(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
