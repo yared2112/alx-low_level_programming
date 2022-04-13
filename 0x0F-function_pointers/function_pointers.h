0f206c0952a4dd508696fa56874e6114915d7939
yared2112 <teklewold.yared21@gmail.com>
2022-04-13 21:49:09 +0300

    completed task

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,12 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FILE_F_POINTRS
+#define FILE_F_POINTRS
 
-void _putchar(char *C);
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
+
 void array_iterator(int *array, size_t size, void (*action)(int));
+
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /*FUNCTION_POINTERS_H*/
+#endif
